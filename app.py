e693d0af3a38f6cb4a946085818b0fae3707a4f3
Raghib Islam <islamraghib3@gmail.com>
2020-06-16 20:08:40 +0545

    Dasboard section completed

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask , render_template ,request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_mail import Mail
+from werkzeug import secure_filename
+import os
 import json 
 
 
@@ -13,6 +15,7 @@ with open('config.json','r') as c:
 local_server =True    
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
+app.config['UPLOAD_FOLDER']=params['upload_location']
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT='465',
@@ -121,6 +124,26 @@ def edit(sno):
         post=Posts.query.filter_by(sno=sno).first()
         return render_template('edit.html',params=params,post=post)
 
+@app.route("/uploader",methods=["Get","POST"])
+def uploader():
+    if ('user' in session and session['user']==params['admin_user']):
+        if(request.method == 'POST'):
+            f=request.files['file1']
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+            return "Uploaded sucessfully"
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect('/dashboard')
+
+@app.route("/delete/<string:sno>", methods=["GET","POST"])
+def delete(sno):
+    if ('user' in session and session['user']==params['admin_user']):
+        post = Posts.query.filter_by(sno = sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')
 
 
 if __name__ == "__main__":
